46c63da06b67b6c6797723bab297823c51157915
KarmaKamikaze <nicolaijgn@gmail.com>
2023-02-21 12:27:55 +0100

    Initial test code for the RFM69HW transceiver

---
+++ RFM69/RFM69.ino
@@ -0,0 +1,72 @@
+#include <SPI.h>
+#include <RH_RF69.h>
+
+#define RF69_FREQ 868
+#define RFM69_INT     3  // 
+#define RFM69_CS      10  //
+#define RFM69_RST     2  // "A"
+#define SERIAL_BAUD 115200
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  while (!Serial) { delay(1); } // wait until serial console is open
+  
+  pinMode(RFM69_RST, OUTPUT);
+
+  Serial.println("Feather RFM69 RX Test!");
+  Serial.println();
+
+  // manual reset
+  digitalWrite(RFM69_RST, LOW);
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  
+  if (!rf69.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  //uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+  //                  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  //rf69.setEncryptionKey(key);
+}
+
+
+void loop() {
+ if (rf69.available()) {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf69.recv(buf, &len)) {
+      if (!len) return;
+      buf[len] = 0;
+      Serial.print("Received [");
+      Serial.print(len);
+      Serial.print("]: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf69.lastRssi(), DEC);
+
+    } else {
+      Serial.println("Receive failed");
+    }
+  }
+}
