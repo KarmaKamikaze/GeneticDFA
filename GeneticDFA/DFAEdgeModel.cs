f9189166ca0dbd913e8b07eb1fe24fdcc108c3b4
Sarmisuper <sarmilan3@gmail.com>
2023-04-01 14:01:13 +0200

    Created model classes in order to represent DFAs

---
+++ GeneticDFA/DFAEdgeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GeneticDFA;
+
+public class DFAEdgeModel
+{
+    public DFAEdgeModel(int id, DFAStateModel source, DFAStateModel target, string input)
+    {
+        ID = id;
+        Source = source;
+        Target = target;
+        Input = input;
+    }
+    
+    public int ID { get; }
+    public DFAStateModel Source { get; set; }
+    public DFAStateModel Target { get; set; }
+    public string Input { get; set; } //Can maybe be made a char, depends on how input alphabet will be defined.
+    public bool IsNonDet { get; set; }
+    
+}
---
+++ GeneticDFA/DFAStateModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneticDFA;
+
+public class DFAStateModel
+{
+    public DFAStateModel(int id, bool isAccept)
+    {
+        ID = id;
+        IsAccept = isAccept;
+    }
+
+    public int ID { get; }
+    public bool IsAccept { get; set; }
+    public bool IsNonDet { get; set; }
+
+}
