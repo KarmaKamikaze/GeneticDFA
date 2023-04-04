1d94701753acd1aff0b49dfc9c36475eb7774cb0
WassawRoki <56611129+WassawRoki@users.noreply.github.com>
2023-04-04 10:08:10 +0200

    Update serial_monitor_to_file.py

--- serial_monitor_to_file.py
+++ serial_monitor_to_file.py
@@ -1,55 +1,53 @@
-#imports
+#imports.
 import serial.tools.list_ports
 from datetime import datetime
 
-#list out all ports in use
+#variables.
 ports: list = serial.tools.list_ports.comports()
 serial_instance = serial.Serial()
 active_port: str = ""
-received_messages: str = ""
-messages_read: int = 0
+received_messages: list = []
 
-
+#print a list of active ports to user.
 for port in ports:
-    #print out all ports
     print(port)
 
-
+#user selects a port.
 value = f'COM{input("Select Port: COM")}'
-#searches the list of ports matching the one picked
+
+#searches the list of ports matching the one selected by the user.
 for i in range(0, len(ports)):
     if str(ports[i]).startswith(value):
         active_port = value
         print(ports[i])
 
-#must be the same baudrate as arduino
+#must be the same baudrate as arduino.
 serial_instance.baudrate = 9600
 serial_instance.port = active_port
 serial_instance.open()
 
+file_name = datetime.now()
+
 #Create a new file for the received serial data.
-file = open(datetime.now()+".txt", "w")
+file = open(file_name+".txt", "w")
 
 while 1:
+    #message is read from serial.
     arduino_data = serial_instance.readline()
-    #add read message to list
-    received_messages += str(arduino_data.decode().rstrip()+"\n")
-    #increase number of messages stored
-    messages_read +=1
     
-    #if we read STOP we stop streaming data to file
+    #add read message to list.
+    received_messages.append((arduino_data.decode().rstrip()))
+    
+    #if we read STOP we streaming all data to file.
     if 'STOP' in str(arduino_data.decode()):
-        file.write(list_of_recieved_messagdes)
-        print("A STOP token has been received. The program will now close and save the file.")
+        for message in received_messages:
+            file.write(message)
+            
+        print(f"A STOP token has been received. And all messages have been written to file: {file_name}")
+        print("The program will now close and save the file.")
         input("Press any key to close.")
         break
     
-    #if we have more than 9 messagdes appended by newline, we write them to file. And clears
-    if messages_read > 9:
-        file.write(received_messages)
-        received_messages = ""
-        messages_read = 0
-    
-#close file when arduino is no longer connected
+#close file and serial stream when arduino is no longer connected.
 serial_instance.close()
 file.close()
