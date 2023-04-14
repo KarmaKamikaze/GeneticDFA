1879b0f97ca10a0b9b8d40a8731763d20271a042
KarmaKamikaze <nicolaijgn@gmail.com>
2023-04-14 10:47:06 +0200

    Add nodes (states) to graph

---
+++ GeneticDFA/Visualization/GraphVisualization.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Rubjerg.Graphviz;
+
+namespace GeneticDFA.Visualization;
+
+public class GraphVisualization
+{
+    private readonly DFAChromosome _chromosome;
+    private readonly int _generationNumber;
+    private List<Node> _nodes = new();
+    private List<Edge> _edges = new();
+
+    public GraphVisualization(DFAChromosome chromosome, int generationNumber)
+    {
+        _chromosome = chromosome;
+        _generationNumber = generationNumber;
+    }
+
+    /// <summary>
+    /// Constructs a graph from the given chromosome's states and edges.
+    /// </summary>
+    private void ConstructGraph()
+    {
+        // Construct the graph root (this is not a node)
+        RootGraph root = RootGraph.CreateNew($"Gen {_generationNumber}", GraphType.Directed);
+
+        // Make each state into a node
+        foreach (DFAState state in _chromosome.States)
+        {
+            // The node names are unique identifiers
+            _nodes.Add(root.GetOrAddNode(state.ID.ToString()));
+        }
+    }
+}
