2941a3411e7b4b17b0000077b506921cee62d8b1
WassawRoki <56611129+WassawRoki@users.noreply.github.com>
2023-04-17 14:37:16 +0200

    Update main.py

--- TestTraceGen/main.py
+++ TestTraceGen/main.py
@@ -3,24 +3,24 @@ from DFA.small import generate_test_traces_for_smalldfa
 from DFA.car_alarm import generate_test_traces_for_caralarmDFA
 from DFA.brigde import generate_test_traces_for_brigdeDFA
 
-def write_traces_to_file(failing_traces:list,passing_traces:list,user_input: str):
-    ''' Writes traces generated to a file '''
-    # Generate filename
-    time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    if user_input == "1":
-        file_name: str = "small-dfa-traces-"+time
-    elif user_input == "2":
-        file_name: str = "car-alarm-dfa-traces-"+time
-    elif user_input == "3":
-        file_name: str = "brigde-dfa-traces-"+time
-    
-def get_input() -> int:
+def get_input():
     while True:
         try:
             user_input = int(input("> "))
             return user_input
         except ValueError:
             print("Error in input. Please try again.")
+
+def write_traces_to_file(failing_traces:list,passing_traces:list,user_input: str):
+    ''' Writes traces generated to a file '''
+    # Generate filename
+    time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    if user_input == 1:
+        file_name: str = "small-dfa-traces-"+time
+    elif user_input == 2:
+        file_name: str = "car-alarm-dfa-traces-"+time
+    elif user_input == 3:
+        file_name: str = "brigde-dfa-traces-"+time
     
     # Write passing traces
     text_to_write: str = "passing = ["
@@ -50,11 +50,11 @@ def test_trace_generation():
     print("How many char long should it be?")
     length_of_trace: int = input()
     
-    if user_input == "1":
+    if user_input == 1:
         traces = generate_test_traces_for_smalldfa(number_of_failing_traces,number_of_passing_traces,length_of_trace)
-    elif user_input == "2":
+    elif user_input == 2:
         traces =generate_test_traces_for_caralarmDFA(number_of_failing_traces,number_of_passing_traces,length_of_trace)
-    elif user_input == "3":
+    elif user_input == 3:
         traces = generate_test_traces_for_brigdeDFA(number_of_failing_traces,number_of_passing_traces,length_of_trace)
     else: 
         print("Wrong input")
