4ecd3d635a44c9361666a34dc25fb5ccb7cd5c3f
KarmaKamikaze <nicolaijgn@gmail.com>
2023-05-02 15:57:46 +0200

    FileSystemWatcher to populate ListBox in UI

--- GeneticDFAUI/ViewModels/VisualizationViewModel.cs
+++ GeneticDFAUI/ViewModels/VisualizationViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Windows.Input;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media.Imaging;
 using GeneticDFA;
@@ -13,34 +15,47 @@ namespace GeneticDFAUI.ViewModels;
 public class VisualizationViewModel : ViewModelBase
 {
     private readonly FileSystemWatcher _watcher;
+    private readonly Setup _geneticAlgorithmThread;
+    private List<string> _generations = new List<string>();
 
     public VisualizationViewModel(Setup geneticAlgorithmThread)
     {
-        GeneticAlgorithmThread = geneticAlgorithmThread;
+        _geneticAlgorithmThread = geneticAlgorithmThread;
         SwitchToSettingsWindow = ReactiveCommand.Create(OnSwitchToSettings);
 
         _watcher = new FileSystemWatcher("./Visualizations/");
+        _watcher.Filter = "*.svg";
+        _watcher.Created += OnGenerationListCreateUpdate;
         _watcher.EnableRaisingEvents = true;
-        _watcher.Created += SetGenerationList;
+        _watcher.IncludeSubdirectories = false;
     }
-    public Setup GeneticAlgorithmThread { get; }
-    public List<string> Generations { get; set; } = new List<string>();
+
+    public List<string> Generations
+    {
+        get => _generations;
+        set => this.RaiseAndSetIfChanged(ref _generations, value);
+    }
 
     public Bitmap Image { get; set; }
 
     public ICommand SwitchToSettingsWindow { get; }
+
     public void OnSwitchToSettings()
     {
-        GeneticAlgorithmThread.Kill();
-        var app = (ClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!;
+        _geneticAlgorithmThread.Kill();
+        var app = (ClassicDesktopStyleApplicationLifetime) Application.Current!.ApplicationLifetime!;
         app.MainWindow.Content = new SettingsView()
         {
             DataContext = new SettingsViewModel(),
         };
     }
 
-    private void SetGenerationList(object sender, FileSystemEventArgs e)
+    private void OnGenerationListCreateUpdate(object sender, FileSystemEventArgs e)
     {
         Generations.Add(e.Name!);
+        var app = (ClassicDesktopStyleApplicationLifetime) Application.Current!.ApplicationLifetime!;
+        ListBox? listbox = app.MainWindow.FindControl<ListBox>("VisualizationList");
+        listbox.Items = Generations;
+        listbox.InvalidateMeasure(); // Queries to reload listbox
     }
 }
